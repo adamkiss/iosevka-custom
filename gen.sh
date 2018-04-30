d1c52007fe12de17f1f26984a57e4271876e8b3d
Adam Kiss <iam@adamkiss.com>
2018-04-30 23:24:18 +0200

    Update Font Name and some characters and prepare to ignore obliques

--- gen.sh
+++ gen.sh
@@ -4,10 +4,10 @@ set -eux
 
 rm -fr ./iosevka-repo/dist/iosevka-custom
 
-family_name='iosevka-ak'
+family_name='Iosevka AK Mono'
 #weights='book' #testing onluy one weight
 weights=''
-design='sans ligset-coq'
+design='sans  ligset-coq'
 upright="ss09 \
 v-i-zshaped v-l-zshaped v-m-shortleg v-q-straight v-t-cross v-y-curly \
 v-zero-slashed v-three-flattop \
@@ -15,15 +15,14 @@ v-tilde-low v-asterisk-low v-paragraph-low v-caret-low v-underscore-hight \
 v-brace-straight v-dollar-through v-numbersign-slanted \
 v-at-long v-eszet-traditional"
 italic="ss09 \
-v-i-hooky v-l-hooky v-t-standard v-y-curly \
+v-a-doublestorey v-f-tailed v-g-doublestorey \
+v-i-zshaped v-l-zshaped v-m-shortleg v-t-cross v-y-curly \
 v-zero-slashed v-three-flattop \
 v-tilde-low v-asterisk-low v-paragraph-low v-caret-low v-underscore-hight \
 v-brace-straight v-dollar-through v-numbersign-slanted \
 v-at-long v-eszet-traditional"
 
 cd iosevka-repo
-make custom-config set='ak' family="$family_name" \
+make custom-config set='ak-mono' family="$family_name" \
 	design="$design" upright="$upright" italic="$italic"
-make custom set='ak'
-
-cd .. && ln -fs ./iosevka-repo/dist/ dist
+make custom set='ak-mono'
